c5c4a8cfff7fca89dcd470eefc4381626c498aa9
Zachary Zadell <zacharyz@gmail.com>
2012-04-27 17:33:11 -0700

    use its own api key

--- flickr.js
+++ flickr.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
     }
   );
   $(".image").each(function() {
-    var apiKey = '844c54d33c43bdde4fa79ce68df6505d';
+    var apiKey = '99ec590d0a9937053b6aa5049ed35e51';
     var photoID = $(this).attr('id');
     var container= $(this);
     $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key='+apiKey+'&photo_id='+photoID+'&format=json&nojsoncallback=1',
       function(data){
         var photo = data.photo;
         var url = 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret+'_b.'+photo.originalformat;
-        container.append('<img src ='+url+'>');
+        container.append('<img class="center" src ='+url+'>');
         container.append("<h2><span>"+photo.title._content+"</span></h2>");
-       });
+      });
    });
 
 });
